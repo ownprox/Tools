5ffb2337c9d79e663c2ea21e3628e8b3cb83625d
OwnProx <ownproxie@outlook.com>
2019-01-05 05:46:45 +0000

    Tools

---
+++ Remote Server Monitor/Remote Server Monitor/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ServerMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Server = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Players = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Server,
+            this.Players,
+            this.Status});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(463, 370);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Server
+            // 
+            this.Server.Text = "Server";
+            this.Server.Width = 330;
+            // 
+            // Players
+            // 
+            this.Players.Text = "Players";
+            this.Players.Width = 66;
+            // 
+            // Status
+            // 
+            this.Status.Text = "Status";
+            this.Status.Width = 63;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 4000;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Server Monitor";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(100, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.showToolStripMenuItem.Text = "Hide";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.ShowToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 370);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Server Monitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Server;
+        private System.Windows.Forms.ColumnHeader Players;
+        private System.Windows.Forms.ColumnHeader Status;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Remote Server Monitor/Remote Server Monitor/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace ServerMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool CanClose = false;
+        bool[] ServersOnline;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(File.Exists("Servers.conf"))
+            using (StreamReader r = new StreamReader("Servers.conf"))
+            {
+                string line;
+                string[] splts;
+                while((line = r.ReadLine()) != null)
+                {
+                    splts = line.Split(',');
+                    listView1.Items.Add(new EpiListStatus(splts[0], splts[1]));
+                }
+            }
+            ServersOnline = new bool[listView1.Items.Count];
+            GetServerStatus();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            GetServerStatus();
+        }
+
+        void GetServerStatus()
+        {
+            int ServerCount = listView1.Items.Count;
+            for (int i = 0; i < ServerCount; i++)
+            {
+                EpiListStatus ELS = (EpiListStatus)listView1.Items[i];
+                try
+                {
+                    new SourceQuery(ELS.Connection, ELS.Port, (SourceQuery.ServerData SrvData) =>
+                    {
+                        ELS.LastUpdate = DateTime.Now.AddMinutes(1);
+                        ELS.UpdateStatus(this, SrvData.Name, SrvData.Players, true);
+                    }).Dispose();
+
+                    if (DateTime.Now.Subtract(ELS.LastUpdate).TotalSeconds > 70)
+                    {
+                        ServersOnline[i] = false;
+                        ELS.UpdateStatus(this, string.Empty, 0, false);
+                    }
+                    else ServersOnline[i] = true;
+                }
+                catch { ServersOnline[i] = false; timer2.Start(); ELS.UpdateStatus(this, string.Empty, 0, false); }
+            }
+
+            bool EnableBeeper = false;
+            for (int i = 0; i < ServerCount; i++) if (!ServersOnline[i]) EnableBeeper = true;
+            if (EnableBeeper) timer2.Start();
+            else timer2.Stop();
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            SystemSounds.Beep.Play();
+        }
+
+        private void ShowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (showToolStripMenuItem.Text == "Hide")
+            {
+                showToolStripMenuItem.Text = "Show";
+                Hide();
+            }
+            else
+            {
+                showToolStripMenuItem.Text = "Hide";
+                Show();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(!CanClose)
+            {
+                showToolStripMenuItem.Text = "Show";
+                Hide();
+                e.Cancel = true;
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CanClose = true;
+            Close();
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (showToolStripMenuItem.Text == "Hide")
+            {
+                showToolStripMenuItem.Text = "Show";
+                Hide();
+            }
+            else
+            {
+                showToolStripMenuItem.Text = "Hide";
+                Show();
+            }
+        }
+    }
+
+    class EpiListStatus : ListViewItem
+    {
+        public DateTime LastUpdate;
+        public string Connection;
+        public int Port;
+        public EpiListStatus(string SrvName, string Connection)
+        {
+            this.Text = SrvName;
+            string[] splts = Connection.Split(':');
+            this.Connection = splts[0];
+            int.TryParse(splts[1], out Port);
+            LastUpdate = DateTime.Now;
+            SubItems.AddRange(new string[] { "", "" });
+            SubItems[1].Text = "0";
+            SubItems[2].Text = "Offline";
+        }
+
+        public void UpdateStatus(Form frm, string SrvName, byte Players, bool Online)
+        {
+            frm.Invoke((MethodInvoker)delegate ()
+            {
+                if(SrvName != string.Empty) SubItems[0].Text = SrvName;
+                SubItems[1].Text = Players.ToString();
+                SubItems[2].Text = Online ? "Online" : "Offline";
+            });
+        }
+    };
+}
